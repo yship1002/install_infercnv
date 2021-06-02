3c785b16a56bdfc06c8de9ed255e36bdc2baf163
Jingzhi Yang <jyang10@cypress01-033.cm.cluster>
2021-06-02 16:52:22 -0500

    Fix how to get path and instead of mv adap I use cp adap

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #Place this script as well as lf_adap.c file and install_infercnv.R in your luster directory
-idev
 module load jags/4.3.0
 module load R/4.1.0-intel
 #Loading jags automatically load gcc/4.7.4 replace it with a later version
 module unload gcc/4.7.4
 export LD_LIBRARY_PATH=/share/apps/anaconda/3/5.1.0/lib:$LD_LIBRARY_PATH
 mkdir -p R/Library
-path=echo pwd
+cur_path=$(pwd)
+a="/lf_adap.c"
+adap_path=${cur_path}${a}
 cd ~
 wget http://cran.r-project.org/src/contrib/locfit_1.5-9.4.tar.gz
 tar -xf locfit_1.5-9.4.tar.gz
@@ -15,7 +16,7 @@ tar -xf locfit_1.5-9.4.tar.gz
 #I have fixed all occrence in the file and replace it with the correct one
 cd locfit/src
 rm lf_adap.c
-mv $path/lf_adap.c .
+cp ${adap_path} .
 cd ~
 R CMD INSTALL locfit
 #Use a pre-written Rscript to continue

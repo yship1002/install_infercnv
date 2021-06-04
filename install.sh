9f83695a9c5eaf1761cf04290e2897f19a2b6cf8
Jingzhi Yang <jyang10@cypress1.cm.cluster>
2021-06-04 01:44:29 -0500

    instead of using relative directory path I replace them with absolute path so that it is easier to debug

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#Place this script as well as lf_adap.c file and install_infercnv.R in your luster directory
 
 
 #Loading necessary modules for installation
@@ -9,37 +8,46 @@ module load R/4.1.0-intel
 #Loading jags automatically load gcc/4.7.4 replace it with a later version
 module unload gcc/4.7.4
 
+#Create R library for all packages 
+mkdir -p ../R/Library
+
+
+#Get current path
+start_path=$(pwd)
+cd ..
+home_path=$(pwd)
+a="/lf_adap.c"
+
+#To calculate adap file path
+adap_path=${start_path}${a}
+
+
 #Set LD_path so that libpng16.so can be loaded
 export LD_LIBRARY_PATH=/share/apps/anaconda/3/5.1.0/lib:$LD_LIBRARY_PATH
 
 #Specify the R library where all R packages will be installed otherwise it$
 #See https://wiki.hpc.tulane.edu/trac/wiki/cypress/InstallingRPackages for$
-export R_LIBS=/lustre/project/wdeng7/R/Library
+cd ${home_path}/R/Library
+export R_LIBS=$(pwd)
 
-#Get current path
-cur_path=$(pwd)
-a="/lf_adap.c"
 
-#To calculate adap file path
-adap_path=${cur_path}${a}
-
-#Changing directory to home to begin installation of locfit one of the dependencies of infercnv
-cd ~
+#Changing directory to your personal directory on luster to begin installation of locfit one of the dependencies of infercnv
+cd ${home_path}
 wget http://cran.r-project.org/src/contrib/locfit_1.5-9.4.tar.gz
 tar -xf locfit_1.5-9.4.tar.gz
 
 #Logical operators in original lf_adap.c file are wrong
 #I have fixed all occrence in the file and replace it with the correct one here
-cd locfit/src
+cd $(home_path)/locfit/src
 rm lf_adap.c
 cp ${adap_path} .
-cd ~
+cd ${home_path}
 
 #Using command line to install R package locfit
 R CMD INSTALL locfit
 
 #Use a pre-written Rscript to continue
 echo "Start of Rscript"
-cd ${cur_path}  #Changing directory to where Rscript is
+cd ${start_path}  #Changing directory to where Rscript is
 Rscript install_infercnv.R
 echo "End of Rscript"
